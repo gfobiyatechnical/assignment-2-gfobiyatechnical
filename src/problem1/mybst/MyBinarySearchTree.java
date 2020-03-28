5d1a7c1850dff4711c0d908ba37b5d0e88eee7e3
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 08:43:21 +0530

    init commit... MyBinarySearchTree.java Constructor

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -4,7 +4,28 @@
  *  Date: 28-Mar-20
  *
  */
+
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+
+    Queue<Integer> queue;
+    private TreeNode newnode;
+    private TreeNode root;
+    private TreeNode temp;
+    private int ctr;
+
+    public MyBinarySearchTree() {
+        temp = null;
+        root = null;
+        ctr = 0;
+        queue = new LinkedList<>();
+    }
+
 }
