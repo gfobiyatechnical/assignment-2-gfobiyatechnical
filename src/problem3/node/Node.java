c2d31e0a4d2aa86b4992befae9e5cffe3b53b5ae
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 11:03:26 +0530

    Problem-3 Node.java  init.commit

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,6 +5,38 @@
  *
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+
+    private Student student;
+    private Node next;
+
+    /*
+     Constructor
+     */
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    /*
+    Getter & Setter methods
+     */
+    public Student getData() {
+        return student;
+    }
+
+    public void setData(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
