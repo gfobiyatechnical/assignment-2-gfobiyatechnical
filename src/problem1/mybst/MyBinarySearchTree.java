cbc347b1bf4ae49495769ca661bd147c4aa3ce8a
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 08:47:28 +0530

    01... MyBinarySearchTree.java getter & setter method updated

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -15,17 +15,42 @@ import java.util.Queue;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
 
-    Queue<Integer> queue;
     private TreeNode newnode;
     private TreeNode root;
     private TreeNode temp;
     private int ctr;
+    Queue<Integer> queue;
 
+    // Default Constructor
     public MyBinarySearchTree() {
         temp = null;
         root = null;
         ctr = 0;
         queue = new LinkedList<>();
     }
+
+    //node generation
+    private void generateNode(int data) {
+        newnode = new TreeNode();
+        newnode.setData(data);
+        newnode.setLeft(null);
+        newnode.setRight(null);
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    //setting root node
+    public void setRoot(int data) {
+        generateNode(data);
+        if (root == null) {
+            root = newnode;
+            temp = root;
+            newnode = null;
+
+        }
+    }
+
 
 }
