c37bd334621339e796df82620a5de89d07390485
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 08:53:24 +0530

    01... MyBinarySearchTree.java Insertion helper method added....

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -11,6 +11,7 @@ import problem1.node.TreeNode;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
@@ -49,6 +50,46 @@ public class MyBinarySearchTree {
             temp = root;
             newnode = null;
 
+        }
+    }
+
+    // Binary Tree: Insertion helper method
+    public void insert(TreeNode tempRoot) {
+        if (newnode == null) {
+            generateNode(new Scanner(System.in).nextInt());
+            System.out.println("Node Generated");
+        }
+        try {
+            if (newnode.getData() <= tempRoot.getData()) {
+                System.out.println("Left Traversal...");
+                if (tempRoot.getLeft() == null) {
+                    tempRoot.setLeft(newnode);
+                    System.out.println("Node inserted at Left");
+                    queue.add(newnode.getData());
+                    ctr--;
+                    newnode = null;
+                    return;
+                } else {
+                    System.out.println("Left not empty changing tempRoot ");
+                    insert(tempRoot.getLeft());
+                }
+            }
+        } catch (NullPointerException ignore) {
+        }
+        try {
+            if (newnode.getData() > tempRoot.getData()) {
+                System.out.println("Right Traversal...");
+                if (tempRoot.getRight() == null) {
+                    tempRoot.setRight(newnode);
+                    System.out.println("Node inserted at Right");
+                    ctr++;
+                    newnode = null;
+                } else {
+                    System.out.println("Right not empty changing tmproot");
+                    insert(tempRoot.getRight());
+                }
+            }
+        } catch (NullPointerException ignore) {
         }
     }
 
