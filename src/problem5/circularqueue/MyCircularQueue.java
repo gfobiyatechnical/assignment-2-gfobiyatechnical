1a49b98cb2b2c6efc5dd5d78db0961e76725e78b
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 10:29:12 +0530

    Problem-5 MyCircularQueue.java enQueue helper method added ...commit

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,7 +5,52 @@
  *
  */
 package problem5.circularqueue;
-//to implement circular queue
+
+import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.ArrayList;
+
+//implementation of  circular queue
 public class MyCircularQueue {
+    private Node front;
+    private int size;
+
+    // Constructor
+    public MyCircularQueue() {
+        front = null;
+        size = 0;
+    }
+
+    // Helper Method : enQueue
+    public void enqueue(ArrayList<Student> addData) {
+        for (Student s : addData
+        ) {
+            if (s.getBackLogCounter() - s.getAppearingCounter() == 0) {
+            } else if (s.getBackLogCounter() - s.getAppearingCounter() > 0) {
+                //code to enqueue
+                Node temp = new Node(s);
+                if (this.front == null) {
+                    front = temp;
+                    temp.setNext(temp);
+                    size++;
+                } else {
+                    temp.setNext(this.front);
+                    Node parser = this.front;
+                    while (parser.getNext() != this.front) {
+                        parser = parser.getNext();
+                    }
+                    parser.setNext(temp);
+                    front = temp;
+                    size++;
+                }
+            } else {
+
+                // Message-display in case of invalid Student Data
+                System.out.println("Given Data is not Valid");
+            }
+        }
+
+    }
 
 }
