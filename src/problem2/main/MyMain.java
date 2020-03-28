2aed0d00956a71d23d6b206ca2bbfae9e4ebc57f
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 10:21:35 +0530

    Problem-5 Node.java ...commit

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,20 @@
  *
  */
 package problem2.main;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+//    public static void main(String[] args)
+//    {
+//        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+//        //Adding 5 Students by hard-coding
+//        //The Queue enqueues them according to ascending Roll Number as given for example in Problem Statement
+//        myPriorityQueue.enqueue(new Node(new Student(15,"wdad")));
+//        myPriorityQueue.enqueue(new Node(new Student(500,"wdad")));
+//        myPriorityQueue.enqueue(new Node(new Student(50,"wdad")));
+//        myPriorityQueue.enqueue(new Node(new Student(11,"wdad")));
+//        myPriorityQueue.enqueue(new Node(new Student(55,"wdad")));
+//        myPriorityQueue.printQueue();
+//    }
 }
