9d2b7cc191a0f68dc7c1b7e2af1c64eaca72a2dc
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 11:05:06 +0530

    Problem-3 MyPriorityQueue.java  01.commit

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,17 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyPriorityQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
 }
