13bcdfc87ed19e06aad51901814bc263f6adff5e
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 08:30:31 +0530

    init commit... TreeNode.java Constructor Added

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,24 @@
 package problem1.node;
 
 public class TreeNode {
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+    private TreeNode next;
+
+    // Parameterized Constructor
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+        next = null;
+    }
+
+    //default constructor
+    public TreeNode() {
+        this.left = null;
+        this.right = null;
+        this.data = 0;
+        next = null;
+    }
 }
