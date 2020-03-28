55c55e12bbca9c96b27249cd4f48c56455cc3940
gfobiyatechnical <Yaduvanshi#008>
2020-03-28 09:02:58 +0530

    final.commit : MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -4,12 +4,25 @@
  *  Date: 28-Mar-20
  *
  */
-package problem1.main;
-// executable class
 
+package problem1.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
+// executable class
 public class MyMain {
     public static void main(String[] args) {
 
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.setRoot(new Scanner(System.in).nextInt());
+        System.out.println("Root " + tree.getRoot().getData());
+
+        for (int i = 0; i < 10; i++) {
+            tree.insert(tree.getRoot());
+        }
+        tree.printLeftChildren();
     }
 }
 
